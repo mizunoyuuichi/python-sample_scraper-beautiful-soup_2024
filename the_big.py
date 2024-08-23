cd13562a02fc183e463702e74347cb0ac6d2cbb6
mizen <yuuch_w@hotmail.com>
2024-08-23 17:59:53 +0900

    added

---
+++ the_big.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# 1 ローカルファイルを指定
+
+list_url_1 = "https://www.mapion.co.jp/phonebook/M02002CL42/"
+list_url_2 = "https://www.mapion.co.jp/phonebook/M02002CL42/2.html"
+list_url_3 = "https://www.mapion.co.jp/phonebook/M02002CL42/3.html"
+response = requests.get(list_url_3)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# 2 繰り返し表示に使われているタグを指定
+articles = soup.find_all('tr')
+
+# 3 ループで配列に抽出
+stores = []
+for article in articles:
+	title = article.find('th').find('a')
+	# find や find_all を重ねて 掘り出していく
+	addres = article.find('td')
+
+	if title is None or addres is None:
+		# タイトルまたは住所がない場合はスキップ
+		continue
+
+	#`{ key: value, ... }`で レコード扱いになるらしい
+	stores.append({ "店舗名": title.text, "住所": addres.text })
+
+# 4 リストをCSVに書き出し
+df = pd.DataFrame(stores)
+csv_path = 'the_big_3.csv'
+df.to_csv(csv_path, index=False, encoding='utf-8')
+print(f"CSVファイルが作成されました：{csv_path}")
+
